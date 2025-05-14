dc9c9ec81cb00fbd9df10e95af7fb7adbefcfa23
orodolfoso <orodolfoso.dev@gmail.com>
2025-05-14 12:23:58 -0300

    Inciando o projeto

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#mobile-btn').on('click', function () {
-        $('#mobile_menu').toggleClass('active');
-        $('#mobile-btn').find('i').toggleClass('fa-x');
-    });
-});
